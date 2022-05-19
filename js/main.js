25a854a6d123e58664bdda1c72b0bb71bfa4331c
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-19 11:54:24 +0200

    function to shuffle the deck

--- js/main.js
+++ js/main.js
@@ -2,18 +2,38 @@
 
 const types = ["spades", "diamonds", "clubs", "hearts"];
 const cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let handPlayer = []
+let handPC = []
 let deck = []
+console.log(cards.length);
+
+//prompt to aks players name ENABLE!
+//let playerName = prompt("Please enter your name")
+//document.getElementById('name').innerHTML = playerName;
+
 
 //Making the deck
 makeDeck()
+
 function makeDeck() {
-    for (let i = 0; i < cards.length; i++) {
-        for (let x = 0; x < types.length; x++) {
-            let card = {Value: cards[x], Suit: types[i]};
+    for (i = 0; i < types.length; i++) {
+        for (x = 0; x < cards.length; x++) {
+            let card = {card: cards[x], type: types[i]};
             deck.push(card);
         }
     }
     return deck;
-
 }
 
+// shuffle the deck (500turns)
+//shuffle();
+function shuffle() {
+
+    for (i = deck.length - 1; i > 0; i--) {
+        let x = Math.floor(Math.random() * i);
+        let result = deck[i];
+        deck[i] = deck[x];
+        deck[x] = result;
+
+    }
+}
