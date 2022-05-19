bd51d1cb152987cb70a15400d0bb20f92730ed0c
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-19 16:29:51 +0200

    function to calculate points after dealing

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 //variable and array list
 
-const types = ["spades", "diamonds", "clubs", "hearts"];
+const icons = ["spades", "diamonds", "clubs", "hearts"];
 const cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 let handPlayer = []
 let handPC = []
 let deck = []
-console.log(cards.length);
+let playerCard = 0;
+let playerCard2 = 0
+let pointsPlayer = 0;
+let cardPc = 0;
+let cardPc2 = 0
+let pointsPc = 0;
 
 //prompt to aks players name ENABLE!
 //let playerName = prompt("Please enter your name")
@@ -16,9 +21,9 @@ console.log(cards.length);
 makeDeck()
 
 function makeDeck() {
-    for (i = 0; i < types.length; i++) {
-        for (x = 0; x < cards.length; x++) {
-            let card = {card: cards[x], type: types[i]};
+    for (let i = 0; i < icons.length; i++) {
+        for (let x = 0; x < cards.length; x++) {
+            let card = {"amount": cards[x], "icon": icons[i]};
             deck.push(card);
         }
     }
@@ -29,41 +34,100 @@ function makeDeck() {
 //shuffle();
 function shuffle() {
 
-    for (i = deck.length - 1; i > 0; i--) {
+    for (let i = deck.length - 1; i > 0; i--) {
         let x = Math.floor(Math.random() * i);
         let result = deck[i];
         deck[i] = deck[x];
         deck[x] = result;
+
 
     }
 }
 
 //dealing the first 2 cards to player and pc (removing them from deck)
 function dealTheHand() {
-    for (i = 0; i < 2; i++) {
-        card = deck.pop();
+    for (let i = 0; i < 2; i++) {
+        deck.pop();
         handPlayer.push(deck[Math.floor(Math.random() * deck.length - 1)])
+        document.getElementById('deckcount').innerHTML = String(deck.length)
+    }
+    playerCard = handPlayer[0].amount;
+    playerCard2 = handPlayer[1].amount;
+    console.log(playerCard);
+    console.log(playerCard2);
 
-    }
-    for (x = 0; x < 2; x++) {
-        card = deck.pop();
+    for (let x = 0; x < 2; x++) {
+        deck.pop();
         handPC.push(deck[Math.floor(Math.random() * deck.length - 1)])
+        document.getElementById('deckcount').innerHTML = String(deck.length)
     }
-    // updatepoints(); //Dont forget to enable!!!!!!!!!!!!!!!!!
+    cardPc = handPC[0].amount;
+    cardPc2 = handPC[1].amount;
+    console.log(cardPc);
+    console.log(cardPc2);
+
+   updatePoints(); //Dont forget to enable!!!!!!!!!!!!!!!!!
     console.log(handPlayer);
     console.log(handPC);
-    console.log(deck.length);
+    console.log(pointsPlayer);
+    console.log(pointsPc);
 }
 
-//adding the 2 cards to the hand of player and pc
-function fillhand() {
-    handPlayer.push(card);
-    handPC.push(card);
-    //document.getElementById('handplayer').innerHTML = handPlayer;
-    //document.getElementById('handpc').innerHTML = handPC;
+//function to add up de points
+
+function updatePoints(){
+
+    //players jack/queen/knight and ACE value card 1
+    if (playerCard === "J" || playerCard === "Q" || playerCard === "K"){
+        playerCard = 10;
+    } else if(playerCard === "A"){
+        if (pointsPlayer <=10){
+            playerCard = 11;
+        }else if (pointsPlayer >10){
+            playerCard = 1;
+        }
+    }
+    //players jack/queen/knight and ACE value card 2
+    if (playerCard2 === "J" || playerCard2 === "Q" || playerCard2 === "K"){
+        playerCard2 = 10;
+    } else if(playerCard2 === "A"){
+        if (pointsPlayer <=10){
+            playerCard2 = 11;
+        }else if (pointsPlayer >10){
+            playerCard2 = 1;
+        }
+    }
+
+    // computers  jack/queen/knight and ACE value card 1
+    if (cardPc === "J" || cardPc === "Q" || cardPc === "K"){
+        cardPc = 10;
+    } else if(cardPc === "A"){
+        if (pointsPc <=10){
+            cardPc = 11;
+        }else if (pointsPc >10){
+            cardPc = 1;
+        }
+    }
+
+    // computers  jack/queen/knight and ACE value card 1
+    if (cardPc2 === "J" || cardPc2 === "Q" || cardPc2 === "K"){
+        cardPc2 = 10;
+    } else if(cardPc2 === "A"){
+        if (pointsPc <=10){
+            cardPc2 = 11;
+        }else if (pointsPc >10){
+            cardPc2 = 1;
+        }
+    }
+    pointsPlayer =Number(playerCard) + Number(playerCard2);
+    pointsPc = Number(cardPc) + Number(cardPc2);
 }
+
+
+
+
 
 //starting the game , pushing start activates following functions: make the deck , shuffle it, deal 2 cards to player and pc
-//document.getElementById('start').addEventListener('click', makeDeck);
+
 document.getElementById('start').addEventListener('click', shuffle);
 document.getElementById('start').addEventListener('click', dealTheHand);
